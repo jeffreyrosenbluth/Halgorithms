258c1f20bc5502522b56cc03e48e43045903d55c
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-11-18 10:50:20 -0500

    union find

---
+++ unionfind/UnionFind.hs
@@ -0,0 +1,64 @@
+----------------------------------------------------------
+-- |
+-- 1.5 Case Study: Union-Find
+-- Weighted quick-union with path compression.
+-- Transcription of "http://algs4.cs.princeton.edu/15uf".
+-- (c) 2014 Jeffrey Rosenbluth
+----------------------------------------------------------
+
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.ST
+import Data.Array.ST
+
+data UF s = UF
+  { iD :: STUArray s Int Int -- parent link (site indexed)
+  , sZ :: STUArray s Int Int -- size of component for roots (site indexed)
+  } deriving (Eq)
+
+find :: UF s -> Int -> ST s Int
+find uf p = do
+  q <- readArray (iD uf) p
+  if q == p
+    then return p
+    -- Follow links to find root.
+    else do
+      r <- find uf q
+      -- Path compression.
+      writeArray (iD uf) p r
+      return r
+
+union :: UF s -> Int -> Int -> ST s ()
+union uf p q = do
+  i <- find uf p
+  j <- find uf q
+  unless (i == j) $ do 
+  szi <- readArray (sZ uf) i
+  szj <- readArray (sZ uf) j
+  -- Make smaller root point to larger one.
+  if szi < szj 
+    then do
+      writeArray (iD uf) i j
+      writeArray (sZ uf) j (szi + szj)
+    else do
+      writeArray (iD uf) j i
+      writeArray (sZ uf) i (szi + szj)
+
+connected :: UF s -> Int -> Int -> ST s Bool
+connected uf p q = (==) <$> find uf p <*> find uf q
+
+tinyUF :: Int -> Int -> Bool
+tinyUF p q = runST $ do
+  ps <- newListArray (0, 9) [0..]
+  ss <- newListArray (0, 9) (repeat 0)
+  let uf    = UF ps ss
+      links = [(4,3), (3,8), (6,5), (9,4), (2,1),(5,0),(7,2),(6,1)]
+  mapM_ (uncurry $ union uf) links
+  connected uf p q
+
+main :: IO ()
+main = do
+  let xs = [(p, q, tinyUF p q) | p <- [0..9], q <- [0..9]] 
+  mapM_ print xs

a1326fd503fdf5234b35069a524e94096491e7d7
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2014-11-18 10:59:42 -0500

    comment unionfind

--- unionfind/UnionFind.hs
+++ unionfind/UnionFind.hs
@@ -18,6 +18,8 @@ data UF s = UF
   , sZ :: STUArray s Int Int -- size of component for roots (site indexed)
   } deriving (Eq)
 
+-- | Returns the component identifier for the component containing
+--   the site.
 find :: UF s -> Int -> ST s Int
 find uf p = do
   q <- readArray (iD uf) p
@@ -30,6 +32,8 @@ find uf p = do
       writeArray (iD uf) p r
       return r
 
+-- | Merges the component containing site p with the
+--   component containing site q.
 union :: UF s -> Int -> Int -> ST s ()
 union uf p q = do
   i <- find uf p
@@ -46,9 +50,12 @@ union uf p q = do
       writeArray (iD uf) j i
       writeArray (sZ uf) i (szi + szj)
 
+-- | Are the two sites connected and in the same component?
 connected :: UF s -> Int -> Int -> ST s Bool
 connected uf p q = (==) <$> find uf p <*> find uf q
 
+-- | Example of tinyUF from:
+--   "http://algs4.cs.princeton.edu/15uf/tinyUF.txt"
 tinyUF :: Int -> Int -> Bool
 tinyUF p q = runST $ do
   ps <- newListArray (0, 9) [0..]
@@ -58,6 +65,7 @@ tinyUF p q = runST $ do
   mapM_ (uncurry $ union uf) links
   connected uf p q
 
+-- | Check each pair of sites in tinyUF.
 main :: IO ()
 main = do
   let xs = [(p, q, tinyUF p q) | p <- [0..9], q <- [0..9]] 
